42ca17d7cff57a0e2c5a728ab03d06e45f432dd9
ULLIM2026 <mapo20212103@gmail.com>
2026-04-17 19:59:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const { useState, useEffect, useMemo, useRef } = React;
 /* ── Firebase 설정 ── */
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,8 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
